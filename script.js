6a7241e628aa0b47730d9f231160a92de6e4bd99
Tanner Bailey <sonictanpat@gmail.com>
2024-08-22 12:50:24 -0500

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+let js = "amazing";
+//if (js === "amazing") alert("JavaScript is FUN");
+
+//40 + 8 + 23 - 18;
+console.log(40 + 8 + 23 - 18);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+// diff variable names 
+// let first = 'Jonas'
+// let firstNamePerson
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = 'Jonas'
+let PI = 3.1415
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
