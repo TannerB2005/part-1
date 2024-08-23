6a7aa235b475ec70589ea5334deb8bb754a91ba7
Tanner Bailey <sonictanpat@gmail.com>
2024-08-23 15:00:52 -0500

    final

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let js = "amazing";
 //if (js === "amazing") alert("JavaScript is FUN");
 
-//40 + 8 + 23 - 18;
-console.log(40 + 8 + 23 - 18);
+/*//40 + 8 + 23 - 18;
+// console.log(40 + 8 + 23 - 18);
 
 console.log("Jonas");
 console.log(23);
 
 let firstName = "Matilda";
-// diff variable names 
+// diff variable names
 // let first = 'Jonas'
 // let firstNamePerson
 
@@ -16,6 +16,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -29,3 +30,186 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob)
+
+
+//Data Types
+
+let javascriptisFun = true;
+console.log(javascriptisFun);
+
+console.log(typeof true);
+console.log(typeof javascriptisFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptisFun = 'YES!';
+console.log(typeof javascriptisFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// let age = number
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+var job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+
+// 009
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+// older or younger
+//                          greater or less than
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+//10
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 -5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+// challenge 1
+
+ let massMark = 78;
+let massJohn = 92;
+
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+let massMark = 95;
+let massJohn = 85;
+
+let markHeight = 1.88;
+let johnHeight = 1.76;
+
+let markBMI = massMark / markHeight ** 2;
+let johnBMI = massJohn / (johnHeight * johnHeight);
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
+
+
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log('Sarah can start driving license 😊');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years 😊`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+*/
+
+// 5 falsy values:
+// 0
+// ''
+// undefined
+// null
+// NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
